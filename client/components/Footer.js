9f824a451a1bf4f79e161a071ac0322b0792457c
Gaurav Singh <gauravnumber@gmail.com>
2021-07-29 11:14:10 +0530

    no error or warning

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { images } from 'Utilities/common'
 
-const Footer = () => (
-  <div className="footer">
-    <img src={images.toskaLogo} alt="toska" />
-  </div>
-)
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2021</em>
+    </div>
+  )
+}
 
 export default Footer
--- client/index.js
+++ client/index.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-import 'Assets/custom.scss'
-import App from 'Components/App'
-import ErrorBoundary from 'Components/ErrorBoundary'
+// import axios from 'axios'
 
-const refresh = () => render(
-  <BrowserRouter>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </BrowserRouter>,
+// const promise = axios.get('http://localhost:3001/notes')
+// promise.then(response => {
+//   const data = response.data
+//   console.log(data)
+// })
+// axios
+//   .get("http://localhost:3001/notes")
+//   .then(response => {
+//     const notes = response.data
+//     console.log(notes);
+//   })
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 )
-
-refresh()
-
-if (module.hot) {
-  module.hot.accept()
-}
