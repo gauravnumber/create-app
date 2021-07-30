3df66ea5fecfa1b7568d27b55b93d3b7d0c4c476
Gaurav Singh <gauravnumber@gmail.com>
2021-07-30 11:59:41 +0530

    frontend Numbers list showed

--- client/App.js
+++ client/App.js
@@ -186,7 +186,7 @@ const App = () => {
           />
         )
 
-        console.log('temp personService.getAll()', temp)
+        // console.log('temp personService.getAll()', temp)
 
         setPerson(temp)
       })
