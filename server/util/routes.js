674422736a37480a6854cc46a5db490815a91118
Gaurav Singh <gauravnumber@gmail.com>
2021-08-01 13:52:50 +0530

    v6

--- server/util/routes.js
+++ server/util/routes.js
@@ -36,7 +36,7 @@ router.get('/health', (request, response) => {
 })
 
 router.get('/version', (request, response) => {
-  response.send('4')
+  response.send('6')
 })
 
 router.delete('/persons/:id', (request, response, next) => {
